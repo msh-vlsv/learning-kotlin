67ac56ad7c4ef4ac79e5ba7dd9a8eacec246d717
msh.vlsv <maria.vlasova.02@gmail.com>
2019-08-08 22:40:09 +0200

    learn variables, constants, and types

---
+++ Sandbox/src/TypeIntro.kt
@@ -0,0 +1,15 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    var hasSteed = false
+    val pubName = "The Unicorn's Horn"
+    val publicanName = "Madrigal"
+    var goldPieces = 50
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+    val reflectedPlayerName = playerName.reversed()
+    println(reflectedPlayerName)
+}
