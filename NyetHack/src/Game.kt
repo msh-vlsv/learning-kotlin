9bdd3927e561a64531cb5ffbe134240e1c40c69d
msh.vlsv <maria.vlasova.02@gmail.com>
2019-08-10 15:30:34 +0200

    learn functions

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -5,10 +5,34 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
 //    Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+//    Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
 
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -20,7 +44,5 @@ fun main(args: Array<String>) {
         else -> "is in awful condition!"
     }
 
-//    Player status
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
-}
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
