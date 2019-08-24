002909b6b80e1adafa6e66d74b5a02a64d81500b
msh.vlsv <maria.vlasova.02@gmail.com>
2019-08-24 13:39:36 +0200

    learn null safety and exceptions

---
+++ Sandbox/src/Tavern.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+//    var beverage = readLine()?.let {
+//        if (it.isNotBlank()) {
+//            it.capitalize()
+//        } else {
+//            "Buttered Ale"
+//        }
+//    }
+
+//    var beverage = readLine()!!.capitalize()
+
+    var beverage = readLine()
+//        beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage is null!!")
+    }
+
+//        beverage = null
+    println(beverage)
+
+
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
+}
