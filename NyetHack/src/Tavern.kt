cd3e6334a5bebbd4da0df871244d162102baff81
msh.vlsv <maria.vlasova.02@gmail.com>
2019-11-14 20:23:29 +0100

    learn strings

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,3 +1,5 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main(args: Array<String>) {
 //    var beverage = readLine()?.let {
 //        if (it.isNotBlank()) {
@@ -19,4 +21,43 @@ fun main(args: Array<String>) {
 
     val beverageServed: String = beverage ?: "Buttered Ale"
     println(beverageServed)
+
+    placeOrder("shandy,Dragon's Breath,5.91")
+
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+//    val data = menuData.split(',')
+//    val type = data[0]
+//    val name = data[1]
+//    val price = data[2]
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+//    val phrase = "Ah, delicious $name"
+//    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name")}"
+    } else {
+        "Madrigal says: Thank's for the $name"
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value.toLowerCase()) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "0" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
